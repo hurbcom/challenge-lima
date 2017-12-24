160e59e10cc0333c3236ce6d3b61aced87503bfb
Matheus Rabelo <matheusfreirerabelo@gmail.com>
2017-12-24 20:26:45 -0200

    Improve code structure

--- source/drone.js
+++ source/drone.js
@@ -5,30 +5,30 @@ const isInMemo = (memo, posX, posY) =>
     (memo[`${posX}x${posY}`] === true);
 
 const rotateRight = (facing) => {
-switch (facing) {
-    case 'L': return 'S';
-    case 'S': return 'O';
-    case 'O': return 'N';
-    default: return 'L';
-}
+    switch (facing) {
+        case 'L': return 'S';
+        case 'S': return 'O';
+        case 'O': return 'N';
+        default: return 'L';
+    }
 };
 
 const rotateLeft = (facing) => {
-switch (facing) {
-    case 'L': return 'N';
-    case 'N': return 'O';
-    case 'O': return 'S';
-    default: return 'L';
-}
+    switch (facing) {
+        case 'L': return 'N';
+        case 'N': return 'O';
+        case 'O': return 'S';
+        default: return 'L';
+    }
 };
 
 const getNextPos = (posX, posY, facing) => {
-switch (facing) {
-    case 'N': return { posX: posX, posY: posY + 1 };
-    case 'S': return { posX: posX, posY: posY - 1 };
-    case 'L': return { posX: posX + 1, posY: posY };
-    case 'O': return { posX: posX - 1, posY: posY };
-}
+    switch (facing) {
+        case 'N': return { posX: posX, posY: posY + 1 };
+        case 'S': return { posX: posX, posY: posY - 1 };
+        case 'L': return { posX: posX + 1, posY: posY };
+        case 'O': return { posX: posX - 1, posY: posY };
+    }
 };
 
 const getValidInstructions = (instructions) => {
@@ -56,10 +56,10 @@ const fly = (gridX, gridY, posX, posY, facing, instructions) => {
             if (pos.posX > 0 && pos.posX <= gridX &&
                 pos.posY > 0 && pos.posY <= gridY &&
                 !isInMemo(memo, pos.posX, pos.posY)) {
-                picturesTaken++;
-                posX = pos.posX;
-                posY = pos.posY;
-                saveInMemo(memo, posX, posY);
+                    picturesTaken++;
+                    posX = pos.posX;
+                    posY = pos.posY;
+                    saveInMemo(memo, posX, posY);
             }
         }
     });
